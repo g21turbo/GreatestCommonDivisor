c59a49550ef65b929cc370966d7080bc3f2c3a99
g21turbo <dg21stew@gmail.com>
2023-11-06 20:37:28 -0800

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,29 @@ public class Main {
 
     public static int getGreatestCommonDivisor(int first, int second) {
 
+        // Check if either of the input numbers is less than 10
         if (first < 10 || second < 10) {
+            // If any of the numbers is less than 10, return -1 (as per the problem's requirement)
             return -1;
         }
 
+        // Determine the smaller of the two input numbers
         int min = first < second ? first : second;
 
+        // Initialize the greatest common divisor (GCD) to 1
         int gcd = 1;
+
+        // Iterate through numbers from 1 up to the minimum of the two input numbers
         for (int i = 1; i <= min; i++) {
-            if (first % i == 0 & second % i == 0) {
+            // Check if 'i' is a common divisor of both 'first' and 'second'
+            if (first % i == 0 && second % i == 0) {
+                // If 'i' is a common divisor, update the GCD to 'i'
                 gcd = i;
             }
         }
+
+        // After iterating through all potential common divisors, return the GCD
         return gcd;
     }
+
 }
